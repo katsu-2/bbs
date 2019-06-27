d9e174c73ea485bea09ca61f40b86c572f516400
大平勝也 <k8t2y9.cat@gmail.com>
2019-06-27 14:15:36 +0900

    style: indexの調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @tweets = Tweet.recent
+    @posts = Post.recent
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
