7242156a5218336dcd372f002581bd0c86a8d52d
大平勝也 <k8t2y9.cat@gmail.com>
2019-06-27 15:09:45 +0900

    feat: 必要ファイルの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.recent
   end
@@ -6,6 +8,8 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def show;end
 
   def create
     @post = Post.new(post_params)
@@ -16,8 +20,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit;end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
